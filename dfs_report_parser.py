6eaebfce33b880a5889fa1d0a8d93741935d8e5f
tanli <tanli@philisence.com>
2021-09-02 00:10:16 +0800

    解析dfs的report，将其导出为csv

---
+++ dfs_report_parser.py
@@ -0,0 +1,23 @@
+# import codecs
+# import builtins
+import re
+
+import pandas as pd
+
+file = open('d:/test/dfstest/dfs_report_20210831.txt')
+flist = []
+fsub = []
+flist = re.split('\n\n', file.read())
+for content in flist:
+    if content and (content.startswith('Name') or 'Name' in content):
+        fsub.append(re.sub(r'\n\n|^\n', '', content))
+
+slist = []
+for d in fsub:
+    dic = dict({i.split(': ')[0]: i.split(': ')[1] for i in d.split('\n')})
+    slist.append(dic)
+ff = pd.DataFrame(slist)
+print(ff)
+# df = pd.concat(pd.DataFrame([l]) for l in slist)
+# print(df)
+# df.to_csv('dfs_report_20210831.csv', index=False, header=True)
